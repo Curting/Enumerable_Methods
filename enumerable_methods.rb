3b962c79da38f2cef6913f98b34a6afbc9a60b34
Oliver <oc@hejverden.dk>
2017-09-12 19:49:12 +0200

    Replicate #each_with_index in #my_each_with_index

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,3 +1,21 @@
 module Enumerable
-  
+
+  def my_each
+    for elements in self
+      yield(elements)
+    end
+  end
+
+  def my_each_with_index
+    n = self.length
+    i = 0
+
+    n.times do |element|
+      yield(self[i], i)
+      i += 1
+    end
+
+    self
+  end
+
 end
